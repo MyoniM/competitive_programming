8a538735485b00f7d96d2ef68f69b6df1101994f
MyoniM <se.yonatan.merkebu@gmail.com>
2022-05-03 21:12:18 +0300

    find_target_indices_after_sorting

--- week1/find_target_indices_after_sorting.py
+++ week1/find_target_indices_after_sorting.py
@@ -1,4 +1,12 @@
 class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
         nums.sort()
-        return [i for i in range(len(nums)) if nums[i] == target]
+        result = []
+        stop = False
+        for i in range(len(nums)):
+            if nums[i] == target:
+                result.append(i)
+                stop = True
+            elif(stop):
+                break
+        return result
