51d1754e6bce2bd809d15a5c685dc5e70e721d0a
MyoniM <se.yonatan.merkebu@gmail.com>
2022-05-03 14:52:49 +0300

    bubble_sort

--- week1/fizz_buzz.py
+++ week1/fizz_buzz.py
@@ -8,13 +8,10 @@ class Solution(object):
         for i in range(1, n+1):
             if(i % 3 == 0 and i % 5 == 0):
                 ans.append("FizzBuzz")
-                continue
             elif(i % 3 == 0):
                 ans.append("Fizz")
-                continue
             elif(i % 5 == 0):
                 ans.append("Buzz")
-                continue
             else:
                 ans.append(str(i))
         return ans
