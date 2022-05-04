26b20d6fa329354e6f48da9bee66c65ba33e45bc
MyoniM <se.yonatan.merkebu@gmail.com>
2022-05-04 16:51:37 +0300

    sort_colors

--- week1/sort_colors.py
+++ week1/sort_colors.py
@@ -3,10 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count = {}
+        count = {0: 0, 1: 0, 2: 0}
         for i in range(len(nums)):
-            if nums[i] not in count: count[nums[i]] = 0
-            count[nums[i]]+=1
-        nums.clear() 
-        for i in [0,1,2]:
-            if i in count: nums +=  [i]*count[i]
+            count[nums[i]] += 1
+        nums.clear()
+        for i in [0, 1, 2]:
+            nums += [i]*count[i]
