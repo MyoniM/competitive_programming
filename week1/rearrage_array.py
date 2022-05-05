7567b3bdedb34607576289e5e85e6dee8eebda40
MyoniM <se.yonatan.merkebu@gmail.com>
2022-05-05 12:50:34 +0300

    rearrage_array

---
+++ week1/rearrage_array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def rearrangeArray(self, nums):
+        nums.sort()
+        for i in range(0,len(nums)-1,2):
+            nums[i],nums[i+1]=nums[i+1],nums[i]
+        return nums
