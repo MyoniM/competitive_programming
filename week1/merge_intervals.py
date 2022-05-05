0066c8cf5a1acf737e67fb32dde8a951e6b1b88f
MyoniM <se.yonatan.merkebu@gmail.com>
2022-05-05 09:32:38 +0300

    merge_intervals

---
+++ week1/merge_intervals.py
@@ -0,0 +1,10 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        result = []
+        for i, p in enumerate(intervals):
+            if not result: result.append(p)
+            elif result[-1][1] >= p[0]:
+                result[-1] = [result[-1][0],result[-1][1] if result[-1][1] > p[1] else p[1]]
+            else: result.append(p)
+        return result
